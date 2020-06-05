3192a175f8bfc805681b09df06071c2bfe3d346b
alabenabdallah <61456720+alabenabdallah@users.noreply.github.com>
2020-06-06 00:16:11 +0100

    Create player.h

---
+++ player.h
@@ -0,0 +1,8 @@
+#include "structs.h"
+
+    extern Gestion jeu;
+    extern Hero player;
+    extern Input input;
+
+   /* Prototype des fonctions externes que nous allons utiliser ensuite */
+    extern SDL_Surface *loadImage(char *name)
